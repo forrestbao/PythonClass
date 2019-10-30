07c747aa69f6b79c04213c2e026c34d6fd9ca66b
Forrest Bao <forrest.bao@gmail.com>
2019-10-30 16:53:44 -0400

    update slide for while-loops and add detailed docstrings for tic-tac-toe project

--- projects/project_2_tic_tac_toe/tic_tac_toe.py
+++ projects/project_2_tic_tac_toe/tic_tac_toe.py
@@ -1,47 +1,34 @@
-
-
-X = [[0,0,0],[0,0,0],[0,0,0]]
-O = [[0,0,0],[0,0,0],[0,0,0]]
-
-
-def add_piece(grid, cord):
-    """Update the grid after computer or user moves 
-    
-    Args:
-        grid: a 3x3 list 
-        cord: a 1x2 list, [x,y]
-    """
-    [x, y] = cord 
-    grid[x][y] = 1 
-    return grid 
     
 def user_moves(X,O):
-    """ask the user for the coordinate to play an X; if there is a piece, warn and ask again until there is no piece
+    """ask the user for the coordinate to place an O; if there is a piece, warn and ask again until there is no piece
     """
     x = input("what row (0-index)? ")
     y = input("what column (0-index)? ")
     
-    # if [x,y] is a cell already occupied by an X or an O, ask the user again. 
-    
-    
+    # add a while-loop here, as long as (x,y) is occupied by an X or an O, ask again. 
+
     return X
 
-
-
 def computer_moves(X, O, counterX, counterY):
-    """return the coordinate that the computer wants to put an O on the grid. 
+    """
+    Let the computer randomly choose a cell on the grid to put an X. The new location must be unoccupied.  
     """
     
     randomX, counterX = cyrandom(counterX)
     randomY, counterY = cyrandom(counterX)
     
-    # use a while here, if the coordinate (randomX, randomY) is occupied.
+    # add a while-loop here, as long as the random coordinate (randomX, randomY) is occupied by an X or an O, regenerate.
     
-    ...
     return X, counterX, counteY 
 
 def judge(X, O):
-    """return 1 is the user won, -1 if the computer won, 0 if tie, and 100 if too early to call.  
+    """
+    Based on the lists X and O, return one of the following judgments:
+    1: tie
+    2: computer (playing X) wins
+    3: human (playing O) wins
+    4: else (the game should keep going )
+
     """
 
 def ko(judgement):
@@ -49,8 +36,9 @@ def ko(judgement):
     
     judgment:
     1: tie
-    2: computer wins
-    3: human wins
+    2: computer (playing X) wins
+    3: human (playing O) wins
+    4: else (the game should keep going )
     
     """
     if judgement==3:
@@ -58,25 +46,63 @@ def ko(judgement):
     elif judgement == 2:
         print ("Sorry, you were defeated by the computer. ")
 
-def print_board(X, O):
-    """Print the board with X's and O's like this:
+    # add code here to finish the cases 
+
+def print_grid(X,O):
+    """print the board wit X's, O's and underlines to visualize the state of the board 
     
-    XOX
-    _O_
-    _XO
     
-    where underlines means unoccupied/unmarked cells. 
-    
+    args:
+        X: a list of 3-element sublists of 1's and 0's
+        O: a list of 3-element sublists of 1's and 0's
+        
+    note:
+        A 1 in X or means an X or O in the corresponding cell in the grid. 0 if no X nor O in the corresponding cell 
+        For example, given the layout 
+        XOX
+        _O_
+        _XO
+        
+        X will be [[1,0,1],[0,0,0],[0,1,0]], and 
+        O will be [[0,1,0],[0,1,0],[0,0,1]]
+        
     """
-    ...
+   
+    for row in range(3):
+        Lthisrow = ""
+        for column in range(3):
+            if X[row][column] == 1:
+                Lthisrow += "X"
+            elif O[row][column] == 1:
+                Lthisrow += "O"
+            else:
+                Lthisrow += "_"
+        print (Lthisrow)
+                
+    return None  
 
 def cyrandom(counter):
+    """A simple random number generator 
+
+    It draws an element from the list L below based on the counter.
+    The counter advances by 1 after the drawing. 
+    If the new counter is greater than 2, reset it to 0. 
+
+    """
+
     L = [1,2,3]
     draw = L[counter]
     counter += 1 
+
+    # add your code here to reset the counter to 0 if counter is greater than 2 
+
+
     return draw, counter 
     
 def game():
+    """This function is complete. No need to modify. 
+    """
+
     counterX, counterY = 0, 2
     while True:
         print_board(X,O)
@@ -95,6 +121,10 @@ def game():
             ko(judgement)
             return 0
 
+# feel free to call any functions above under this line 
 if __name__ == "__main__":
+
+    X = [[0,0,0],[0,0,0],[0,0,0]]
+    O = [[0,0,0],[0,0,0],[0,0,0]]
     game()
 
